c64bbe8d6b488b49b0d2a3cdae39ff48c32d4bfd
noobow34 <skywind.hk34@gmail.com>
2019-01-26 21:28:18 +0900

    運用状況コード定数追加

--- ja-fleet.Commons/Constants/OperationCode.cs
+++ ja-fleet.Commons/Constants/OperationCode.cs
@@ -3,6 +3,13 @@ namespace jafleet.Commons.Constants
 {
     public static class OperationCode
     {
+        /// <summary>
+        /// 予約登録
+        /// </summary>
+        public const string RESERVED = "0";
+        /// <summary>
+        /// 退役（未抹消）
+        /// </summary>
         public const string RETIRE_UNREGISTERED = "8";
     }
 }
