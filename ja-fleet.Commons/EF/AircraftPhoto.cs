4b8c044adc1a5c63988b25a516ad06c345e66bff
noobow34 <skywind.hk34@gmail.com>
2019-07-16 15:07:17 +0900

    AircraftPhoto追加

---
+++ ja-fleet.Commons/EF/AircraftPhoto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jafleet.Commons.EF
+{
+    [Table("AIRCRAFT_PHOTO")]
+    public class AircraftPhoto
+    {
+        [Key]
+        [Column("REGISTRATION_NUMBER")]
+        public string RegistrationNumber { get; set; }
+
+        [Column("PHOTO_URL")]
+        public string PhotoUrl { get; set; }
+
+        [Column("LAST_ACCESS")]
+        public DateTime LastAccess { get; set; }
+    }
+}
