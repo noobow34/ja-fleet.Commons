74fcffbd498fb0ecc0fc627bc096fa1725484332
noobow34 <skywind.hk34@gmail.com>
2020-08-14 16:31:02 +0900

    AppInfo追加

---
+++ ja-fleet.Commons/EF/AppInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jafleet.Commons.EF
+{
+    [Table("APP_INFO")]
+    public class AppInfo
+    {
+        [Key]
+        [Column("COMMIT_HASH")]
+        public string CommitHash { get; set; }
+        [Column("COMMIT_DATE")]
+        public DateTime CommitDate { get; set; }
+    }
+}
--- ja-fleet.Commons/EF/jafleetContext.cs
+++ ja-fleet.Commons/EF/jafleetContext.cs
@@ -26,6 +26,7 @@ namespace jafleet.Commons.EF
         public virtual DbSet<SeatConfiguration> SeatConfigration { get; set; }
         public virtual DbSet<WorkingStatus> WorkingStatus { get; set; }
         public virtual DbSet<AccessLog> AccessLog { get; set; }
+        public virtual DbSet<AppInfo> AppInfo { get; set; }
 
         public jafleetContext(DbContextOptions<jafleetContext> options) : base(options) { }
 
