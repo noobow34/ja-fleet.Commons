3c0a0f5e41a17aaa7fe22b238b5781be79231f2f
noobow34 <skywind.hk34@gmail.com>
2024-01-15 19:52:15 +0900

    プロフィール画像保存を廃止

--- ja-fleet.Commons/EF/jafleetContext.cs
+++ ja-fleet.Commons/EF/jafleetContext.cs
@@ -19,7 +19,6 @@ namespace jafleet.Commons.EF
         public virtual DbSet<AdminUser> AdminUser { get; set; }
         public virtual DbSet<TypeDetailView> TypeDetailView { get; set; }
         public virtual DbSet<Message> Messages { get; set; }
-        public virtual DbSet<LineUserProfileImage> LineUserProfileImage { get; set; }
         public virtual DbSet<AircraftHistory> AircraftHistory { get; set; }
         public virtual DbSet<AircraftHistoryView> AircraftHistoryView { get; set; }
         public virtual DbSet<AircraftPhoto> AircraftPhoto { get; set; }
