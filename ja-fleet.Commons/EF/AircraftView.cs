d4d488bda17f9eee6809b2c2364330ede4ef6ec5
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-01-09 12:46:34 +0900

    Jsonの依存関係を変更

--- ja-fleet.Commons/EF/AircraftView.cs
+++ ja-fleet.Commons/EF/AircraftView.cs
@@ -1,15 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace jafleet.Commons.EF
 {
+    [Keyless]
     [Table("aircraft_view")]
     public partial class AircraftView:AircraftViewBase
     {
-        [Column("registration_number")]
-        [Key]
-        public new string RegistrationNumber { get; set; }
-
         [Column("photo_direct_url")]
         public string PhotoDirectUrl { get; set; }
     }
