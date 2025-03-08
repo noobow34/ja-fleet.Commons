c41c03f19179cc509abd552d14c8d7defc23d8e1
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-03-08 12:45:17 +0900

    退役機体の写真更新をジョブ化

---
+++ ja-fleet.Commons/Constants/FlightradarConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jafleet.Commons.Constants
+{
+    public static class FlightradarConstant
+    {
+        public const string FR24_DATA_URL = @"https://www.flightradar24.com/data/aircraft/";
+    }
+}
