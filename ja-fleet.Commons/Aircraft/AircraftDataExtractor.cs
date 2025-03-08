133111ae97ce2e560a492b26f2f6e38ef545902e
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-03-08 11:57:33 +0900

    写真取得の分離

---
+++ ja-fleet.Commons/Aircraft/AircraftDataExtractor.cs
@@ -0,0 +1,53 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.XPath;
+using jafleet.Commons.Dto;
+using System.Text.RegularExpressions;
+
+namespace jafleet.Commons.Aircraft
+{
+    public static class AircraftDataExtractor
+    {
+        public static AircraftPhotoResult ExtractPhotoDataFromJetphotos(IHtmlDocument? doc)
+        {
+            string? jetphotos = RemoveQuery(((IHtmlAnchorElement?)doc?.Body?.SelectSingleNode("//*[@id=\"cnt-data-content\"]/div[1]/div[2]/div/div[1]/a"))?.Href);
+            string? photoSmall = null;
+            string? photoLarge = null;
+            if (!string.IsNullOrEmpty(jetphotos))
+            {
+                photoSmall = RemoveQuery(((IHtmlImageElement?)doc?.Body?.SelectSingleNode("//*[@id=\"cnt-data-content\"]/div[1]/div[2]/div/div[1]/a/img"))?.Source);
+                bool replaced = false;
+                photoLarge = Regex.Replace(photoSmall!, @"/\d+/", match =>
+                {
+                    if (!replaced)
+                    {
+                        replaced = true;
+                        return "/full/";
+                    }
+                    return match.Value;
+                });
+            }
+
+            return new AircraftPhotoResult
+            {
+                PhotoUrl = jetphotos,
+                PhotoDirectLarge = photoLarge,
+                PhotoDirectSmall = photoSmall
+            };
+        }
+
+        private static string? RemoveQuery(string? url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return url;
+            }
+
+            int queryIndex = url.IndexOf('?');
+            if (queryIndex >= 0)
+            {
+                return url.Substring(0, queryIndex);
+            }
+            return url;
+        }
+    }
+}
---
+++ ja-fleet.Commons/Dto/AircraftPhotoResult.cs
@@ -0,0 +1,12 @@
+﻿namespace jafleet.Commons.Dto
+{
+    public class AircraftPhotoResult
+    {
+        public string? PhotoUrl { get; set; }
+
+        public string? PhotoDirectLarge { get; set; }
+
+        public string? PhotoDirectSmall { get; set; }
+
+    }
+}
