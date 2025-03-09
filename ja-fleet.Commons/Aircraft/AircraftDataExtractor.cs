3c4c85ddc83d1c1b370201537936234e8c54fcc5
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-03-09 23:36:14 +0900

    写真取得処理変更・追加

--- ja-fleet.Commons/Aircraft/AircraftDataExtractor.cs
+++ ja-fleet.Commons/Aircraft/AircraftDataExtractor.cs
@@ -1,14 +1,65 @@
-﻿using AngleSharp.Dom;
+﻿using AngleSharp;
+using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
 using AngleSharp.XPath;
 using jafleet.Commons.Dto;
+using jafleet.Commons.EF;
 using System.Text.RegularExpressions;
 
 namespace jafleet.Commons.Aircraft
 {
     public static class AircraftDataExtractor
     {
-        public static AircraftPhotoResult ExtractPhotoDataFromJetphotos(IDocument? doc)
+        public static async Task<AircraftPhotoResult?> GetAircraftPhotoAnyRegistrationNumberAsync(string registrationNumber,JafleetContext context)
+        {
+            //写真登録されている場合
+            var ap = context.AircraftPhotos.Where(ap => ap.RegistrationNumber == registrationNumber).SingleOrDefault();
+            if (ap != null && !string.IsNullOrEmpty(ap.PhotoDirectLarge)) {
+                return new AircraftPhotoResult
+                {
+                    PhotoUrl = ap.PhotoUrl,
+                    PhotoDirectLarge = ap.PhotoDirectLarge,
+                    PhotoDirectSmall = ap.PhotoDirectSmall
+                };
+            }
+
+            //登録されていない場合検索
+            string searchUrl = $"https://www.airliners.net/search?registrationActual={registrationNumber}&sortBy=datePhotographedYear&sortOrder=desc&perPage=1";
+            string photoUrl = string.Empty;
+            try
+            {
+                IBrowsingContext bContext = BrowsingContext.New(Configuration.Default.WithDefaultLoader().WithXPath());
+                var htmlDocument = await bContext.OpenAsync(searchUrl);
+                var photos = htmlDocument.Body.SelectNodes(@"//*[@id='layout-page']/div[2]/section/section/section/div/section[2]/div/div[1]/div/div[1]/div[1]/div[1]/a");
+                if (photos.Count != 0)
+                {
+                    string photoNumber = photos[0].TextContent.Replace("\n", string.Empty).Replace(" ", string.Empty).Replace("#", string.Empty);
+                    string newestPhotoLink = $"https://www.airliners.net/photo/{photoNumber}";
+                    var htmlDocument2 = await bContext.OpenAsync(newestPhotoLink);
+                    var photos2 = htmlDocument2.Body.SelectNodes(@"//*[@id='layout-page']/div[5]/section/section/section/div/div/div[1]/div/a[1]/img");
+                    if (photos2.Count != 0)
+                    {
+                        Uri photoUri = new(((IHtmlImageElement)photos2[0]).Source!);
+                        photoUrl = photoUri.OriginalString.Replace(photoUri.Query, string.Empty); ;
+                        return new AircraftPhotoResult
+                        {
+                            PhotoUrl = newestPhotoLink,
+                            PhotoDirectLarge = photoUrl,
+                            PhotoDirectSmall = photoUrl
+                        };
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+
+            //それでも見つからなければnull
+            return null;
+        }
+
+        public static AircraftPhotoResult? ExtractPhotoDataFromJetphotos(IDocument? doc)
         {
             string? jetphotos = RemoveQuery(((IHtmlAnchorElement?)doc?.Body?.SelectSingleNode("//*[@id=\"cnt-data-content\"]/div[1]/div[2]/div/div[1]/a"))?.Href);
             string? photoSmall = null;
@@ -26,14 +77,17 @@ namespace jafleet.Commons.Aircraft
                     }
                     return match.Value;
                 });
+                return new AircraftPhotoResult
+                {
+                    PhotoUrl = jetphotos,
+                    PhotoDirectLarge = photoLarge,
+                    PhotoDirectSmall = photoSmall
+                };
             }
-
-            return new AircraftPhotoResult
+            else
             {
-                PhotoUrl = jetphotos,
-                PhotoDirectLarge = photoLarge,
-                PhotoDirectSmall = photoSmall
-            };
+                return null;
+            }
         }
 
         private static string? RemoveQuery(string? url)
