faa31b9d7c78d47bc93110e63e76af6fdadeed1f
Hiroaki KAWASHIMA <skywind.hk34@gmail.com>
2025-03-10 19:43:35 +0900

    Viewによる処理効率化、不要処理削除

--- ja-fleet.Commons/EF/AircraftView.cs
+++ ja-fleet.Commons/EF/AircraftView.cs
@@ -7,5 +7,13 @@ namespace jafleet.Commons.EF
     [Table("aircraft_view")]
     public partial class AircraftView : AircraftViewBase
     {
+        [Column("photo_url")]
+        public string? PhotoUrl { get; set; }
+
+        [Column("photo_direct_large")]
+        public string? PhotoDirectLarge { get; set; }
+
+        [Column("photo_direct_small")]
+        public string? PhotoDirectSmall { get; set; }
     }
 }
