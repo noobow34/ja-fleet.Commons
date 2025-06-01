6233224f1d612a8c17e1e27041eeacefb472b9a5
noobow34 <skywind.hk34@gmail.com>
2025-06-02 08:27:58 +0900

    不要なusingの削除

--- ja-fleet.Commons/Constants/FlightradarConstant.cs
+++ ja-fleet.Commons/Constants/FlightradarConstant.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace jafleet.Commons.Constants
+﻿namespace jafleet.Commons.Constants
 {
     public static class FlightradarConstant
     {
